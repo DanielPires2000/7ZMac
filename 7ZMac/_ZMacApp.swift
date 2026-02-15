a612798c15be6de859451fd63018c0ea87e0560c
DanielPires2000 <daniel2000work@gmail.com>
2026-02-15 20:13:17 +0000

    Initial Commit

---
+++ 7ZMac/_ZMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ZMacApp.swift
+//  7ZMac
+//
+//  Created by Daniel Pires on 15/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct _ZMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
